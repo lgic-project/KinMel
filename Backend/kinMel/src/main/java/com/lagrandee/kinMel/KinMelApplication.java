c9ec34f04daa2b96072404adf074ba16f6de775d
Suman Devkota <sumandevkota75@gmail.com>
2024-04-19 19:29:48 +0545

    Added spring boot template and folder structure with required dependencies

---
+++ Backend/kinMel/src/main/java/com/lagrandee/kinMel/KinMelApplication.java
@@ -0,0 +1,13 @@
+package com.lagrandee.kinMel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinMelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinMelApplication.class, args);
+	}
+
+}
---
+++ Backend/kinMel/src/test/java/com/lagrandee/kinMel/KinMelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagrandee.kinMel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinMelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
