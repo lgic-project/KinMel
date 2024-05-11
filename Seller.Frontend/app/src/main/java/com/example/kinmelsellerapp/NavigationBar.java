e428ab73e9f736627765db9fbfd86ce87a9a94d1
arpan-asus <iamarpan15@gmail.com>
2024-05-11 11:21:53 +0545

    navigation_bar issue solved (working 100%)

--- Seller.Frontend/app/src/main/java/com/example/kinmelsellerapp/NavigationBar.java
+++ Seller.Frontend/app/src/main/java/com/example/kinmelsellerapp/NavigationBar.java
@@ -1,17 +1,17 @@
 package com.example.kinmelsellerapp;
 import android.os.Bundle;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 public class NavigationBar extends AppCompatActivity {
-    ImageButton btnAddProduct,btnAddCategory,btnListProduct,btnSetting;
+    ImageView btnAddProduct,btnAddCategory,btnListProduct,btnSetting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navigation_bar);
-        getSupportFragmentManager().beginTransaction().replace(R.id.navigation_bar, new Add_Product()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Add_Product()).commit();
 
         btnAddProduct = findViewById(R.id.btnAddProduct);
         btnAddCategory = findViewById(R.id.btnAddCategory);
@@ -20,22 +20,22 @@ public class NavigationBar extends AppCompatActivity {
         btnAddProduct.setOnClickListener(v -> {
             Fragment fragment = new Add_Product();
             FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().addToBackStack(null).replace(R.id.navigation_bar, fragment).commit();
+            fragmentManager.beginTransaction().addToBackStack(null).replace(R.id.fragment_container, fragment).commit();
         });
         btnAddCategory.setOnClickListener(v -> {
             Fragment fragment = new RequestCategory();
             FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().addToBackStack(null).replace(R.id.navigation_bar, fragment).commit();
+            fragmentManager.beginTransaction().addToBackStack(null).replace(R.id.fragment_container, fragment).commit();
         });
         btnListProduct.setOnClickListener(v -> {
             Fragment fragment = new ProductList();
             FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().addToBackStack(null).replace(R.id.navigation_bar, fragment).commit();
+            fragmentManager.beginTransaction().addToBackStack(null).replace(R.id.fragment_container, fragment).commit();
         });
         btnSetting.setOnClickListener(v -> {
             Fragment fragment = new SettingsPage();
             FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().addToBackStack(null).replace(R.id.navigation_bar, fragment).commit();
+            fragmentManager.beginTransaction().addToBackStack(null).replace(R.id.fragment_container, fragment).commit();
         });
     }
 }
