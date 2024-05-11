60e4328aba0cdb5e11dd9c6895ea4322efbd4bbf
navina <navinamogar888@gmail.com>
2024-05-11 12:03:13 +0545

    fragmentation for home page (buyer)

---
+++ Buyer.Frontend/app/src/main/java/com/example/kinmel/Home.java
@@ -0,0 +1,22 @@
+package com.example.kinmel;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Home extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_main, container, false);
+    }
+}
