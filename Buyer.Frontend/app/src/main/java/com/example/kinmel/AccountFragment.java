79bdc3e3ec95cbf303d0da5c33dc200c7f7651d2
navina <navinamogar888@gmail.com>
2024-05-11 12:19:09 +0545

    fragmentation for navigation (buyer)

--- Buyer.Frontend/app/src/main/java/com/example/kinmel/AccountFragment.java
+++ Buyer.Frontend/app/src/main/java/com/example/kinmel/AccountFragment.java
@@ -8,15 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-public class Home extends Fragment {
-
+public class AccountFragment extends Fragment {
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_main, container, false);
+        return inflater.inflate(R.layout.fragment_account, container, false);
     }
 }
