476f0997415b6e834bfc4953df49b70ef10e4d8c
arpan-asus <iamarpan15@gmail.com>
2024-05-11 09:05:03 +0545

    seller login and signup page using activity

--- Seller.Frontend/app/src/main/java/com/example/kinmelsellerapp/SellerLogin.java
+++ Seller.Frontend/app/src/main/java/com/example/kinmelsellerapp/SellerLogin.java
@@ -1,6 +1,26 @@
 package com.example.kinmelsellerapp;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class SellerLogin extends Activity {
+public class SellerLogin extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sellersignin);
+
+        TextView gotoSignUpPage = findViewById(R.id.sign_up);
+        gotoSignUpPage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Start BuyerSignUp activity
+                Intent intent = new Intent(SellerLogin.this, SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
 }
--- Seller.Frontend/app/src/main/java/com/example/kinmelsellerapp/SignUpActivity.java
+++ Seller.Frontend/app/src/main/java/com/example/kinmelsellerapp/SignUpActivity.java
@@ -1,6 +1,25 @@
 package com.example.kinmelsellerapp;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sellersignup);
 
-public class SignUpActivity extends Activity {
+        TextView gotoSignInPage = findViewById(R.id.sign_in);
+        gotoSignInPage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Start BuyerSignUp activity
+                Intent intent = new Intent(SignUpActivity.this, SellerLogin.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
 }
