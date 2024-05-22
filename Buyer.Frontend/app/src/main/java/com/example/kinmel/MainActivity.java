0bb23c16a4eec83289875bcf81cfa93512134256
arpan-asus <iamarpan15@gmail.com>
2024-05-22 09:57:47 +0545

    viewall product page

--- Buyer.Frontend/app/src/main/java/com/example/kinmel/MainActivity.java
+++ Buyer.Frontend/app/src/main/java/com/example/kinmel/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.kinmel;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +12,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        TextView viewAllTextView = findViewById(R.id.viewall);
+        viewAllTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ViewAllActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
---
+++ Buyer.Frontend/app/src/main/java/com/example/kinmel/ViewAllActivity.java
@@ -0,0 +1,19 @@
+package com.example.kinmel;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ViewAllActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_all);
+
+    }
+}
