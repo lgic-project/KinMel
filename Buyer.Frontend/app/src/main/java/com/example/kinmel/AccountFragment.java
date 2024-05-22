6804ec0ab3429342a9b19148639c7af09afcb8ac
arpan-asus <iamarpan15@gmail.com>
2024-05-22 11:03:46 +0545

    changes in fragment file for navigating from one page to another

--- Buyer.Frontend/app/src/main/java/com/example/kinmel/AccountFragment.java
+++ Buyer.Frontend/app/src/main/java/com/example/kinmel/AccountFragment.java
@@ -1,20 +1,39 @@
 package com.example.kinmel;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class AccountFragment extends Fragment {
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.buyersetting, container, false);
+        View view = inflater.inflate(R.layout.buyersetting, container, false);
+
+        TextView tvProfileSetting = view.findViewById(R.id.tvProfileSetting);
+        tvProfileSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), UpdateProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView btnUpdatePassword = view.findViewById(R.id.btnUpdatePassword);
+        btnUpdatePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), UpdatePasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
--- Buyer.Frontend/app/src/main/java/com/example/kinmel/SettingsPage.java
+++ Buyer.Frontend/app/src/main/java/com/example/kinmel/SettingsPage.java
@@ -13,25 +13,5 @@ public class SettingsPage extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.buyersetting);
-
-        TextView profileSettingTextView = findViewById(R.id.tvProfileSetting);
-        profileSettingTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SettingsPage.this, UpdateProfileActivity.class);
-                startActivity(intent);
-
-            }
-        });
-
-        TextView updatePasswordTextView = findViewById(R.id.btnUpdatePassword);
-        updatePasswordTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SettingsPage.this, UpdatePasswordActivity.class);
-                startActivity(intent);
-
-            }
-        });
-   }
+    }
 }
