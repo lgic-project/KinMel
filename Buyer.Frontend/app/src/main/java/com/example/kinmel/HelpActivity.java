2109f77f9f47abd1a9fb1dab3392febf2199d425
navina <navinamogar888@gmail.com>
2024-05-30 13:33:31 +0545

    help page (buyer)

--- Buyer.Frontend/app/src/main/java/com/example/kinmel/HelpActivity.java
+++ Buyer.Frontend/app/src/main/java/com/example/kinmel/HelpActivity.java
@@ -1,6 +1,12 @@
 package com.example.kinmel;
 
-public class HelpActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-
-}
+public class HelpActivity extends AppCompatActivity{
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_help);
+        }
+    }
