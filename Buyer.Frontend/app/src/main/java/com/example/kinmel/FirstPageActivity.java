b2c6eab81aa5e3c3393348bd92963dce10197ef5
navina <navinamogar888@gmail.com>
2024-05-30 15:50:47 +0545

    firstpage to signinpage(buyer)

--- Buyer.Frontend/app/src/main/java/com/example/kinmel/FirstPageActivity.java
+++ Buyer.Frontend/app/src/main/java/com/example/kinmel/FirstPageActivity.java
@@ -1,5 +1,27 @@
 package com.example.kinmel;
 
-public class FirstPageActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstPageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.firstpageactivity);
+
+        Button button = findViewById(R.id.signInButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start BuyerSignUp activity
+                Intent intent = new Intent(FirstPageActivity.this, BuyerLogin.class);
+                startActivity(intent);
+            }
+        });
+    }
 
 }
