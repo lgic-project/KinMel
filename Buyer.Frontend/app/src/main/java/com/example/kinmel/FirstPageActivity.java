8d9aa403bdf7b3a2f4efc3be0b5c0ce57f61a3e3
arpan-asus <iamarpan15@gmail.com>
2024-05-30 16:00:03 +0545

    change in manifest file

--- Buyer.Frontend/app/src/main/java/com/example/kinmel/FirstPageActivity.java
+++ Buyer.Frontend/app/src/main/java/com/example/kinmel/FirstPageActivity.java
@@ -2,9 +2,7 @@ package com.example.kinmel;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FirstPageActivity extends AppCompatActivity {
@@ -14,14 +12,10 @@ public class FirstPageActivity extends AppCompatActivity {
         setContentView(R.layout.firstpageactivity);
 
         Button button = findViewById(R.id.signInButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Start BuyerSignUp activity
-                Intent intent = new Intent(FirstPageActivity.this, BuyerLogin.class);
-                startActivity(intent);
-            }
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(FirstPageActivity.this, BuyerLogin.class);
+            startActivity(intent);
+            finish();
         });
     }
-
 }
