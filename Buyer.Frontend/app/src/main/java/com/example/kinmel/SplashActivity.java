63e1e08906181b830c3c3142564e8371557a6324
navina <navinamogar888@gmail.com>
2024-05-31 17:21:52 +0545

    removed the starting page

--- Buyer.Frontend/app/src/main/java/com/example/kinmel/SplashActivity.java
+++ Buyer.Frontend/app/src/main/java/com/example/kinmel/SplashActivity.java
@@ -31,7 +31,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashActivity.this, FirstPageActivity.class);
+                Intent i = new Intent(SplashActivity.this, BuyerLogin.class);
                 startActivity(i);
                 finish();
             }
